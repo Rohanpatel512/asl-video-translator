5510cc71e4869fab9dceb4d9da0283aa9f2862d2
Rohanpatel512 <rohanpat223@gmail.com>
2026-06-17 20:43:32 -0400

    Implemented frontend translator interface.

---
+++ app/frontend/js/main.js
@@ -0,0 +1,58 @@
+const video = document.getElementById("video");
+const startBtn = document.getElementById("startBtn");
+const recordBtn = document.getElementById("recordBtn");
+
+let stream;
+let mediaRecorder;
+let recordedChunks = [];
+
+startBtn.addEventListener("click", async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    });
+
+   console.log("2", stream);
+
+    video.srcObject = stream;
+
+    console.log("3", recordBtn);
+
+    recordBtn.classList.add("active");
+    recordBtn.disabled = false;
+    
+    console.log("4");
+});
+
+recordBtn.addEventListener("click", () => {
+    if (!stream) return;
+
+    if (recordBtn.textContent === "Start Recording") {
+        recordedChunks = [];
+
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = (e) => {
+            if (e.data.size > 0) {
+                recordedChunks.push(e.data);
+            }
+        };
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(recordedChunks, { type: "video/webm" });
+            const url = URL.createObjectURL(blob);
+
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "hand-sign-recording.webm";
+            a.click();
+        };
+
+        mediaRecorder.start();
+        recordBtn.textContent = "Stop Recording";
+
+    } else {
+        mediaRecorder.stop();
+        recordBtn.textContent = "Start Recording";
+    }
+});
